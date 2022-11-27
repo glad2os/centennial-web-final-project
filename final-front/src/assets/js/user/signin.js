42cf279899c4adf7d2764088effe6345cc5eeb2f
glad2os <lite.console4bit@gmail.com>
2022-11-27 13:07:49 -0500

    sign up form

--- final-front/src/assets/js/user/signin.js
+++ final-front/src/assets/js/user/signin.js
@@ -2,7 +2,24 @@ import {postData} from "../functions";
 import {getCookie, setCookie} from "../cookies";
 
 export async function signup() {
+    let login = document.getElementById('login');
+    let password = document.getElementById('password');
+    let submit = document.getElementById('submit');
 
+    submit.onclick = async () => {
+        if (login.value.length < 4 || login.value.length < 4) {
+            alert("Data length < 4");
+            return;
+        }
+
+        const data = await postData('/users/register', {"login": login.value, "password": password.value});
+
+        if (data.status === 200) {
+            window.location = '/signin'
+        } else {
+            alert("Not able to create an account")
+        }
+    }
 }
 
 export async function signin() {
