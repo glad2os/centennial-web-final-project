9bd5de4be585db9341c7d388b09851b278d22869
glad2os <lite.console4bit@gmail.com>
2022-12-04 17:47:26 -0500

    removing survey

--- final-front/src/assets/js/surveys/get_all_surveys.js
+++ final-front/src/assets/js/surveys/get_all_surveys.js
@@ -17,24 +17,51 @@ async function get_all_surveys() {
 
         let survey = document.createElement('div');
         survey.classList.add('survey');
-        survey.onclick = () => window.location.href = `/survey/${surveyData._id}`;
+        survey.onclick = (ev) => {
+            if (ev.target.classList.contains('survey')
+                || ev.target.classList.contains('title')
+                || ev.target.classList.contains('comments')) window.location.href = `/survey/${surveyData._id}`;
+            else console.log(ev.target)
+        }
         let title = document.createElement('div');
         title.classList.add('title');
         title.innerText = surveyData.topic;
         let comments = document.createElement('div');
         comments.classList.add('comments');
+        let remove = document.createElement('div');
+        remove.classList.add('remove');
         let like = document.createElement('div');
         like.classList.add('like');
-        let dislike = document.createElement('div');
-        dislike.classList.add('dislike');
 
-        comments.insertAdjacentElement('beforeend', dislike);
+        remove.onclick = (ev) => {
+            askForRemove(surveyData._id, survey);
+        }
         comments.insertAdjacentElement('beforeend', like);
+        comments.insertAdjacentElement('beforeend', remove);
 
         survey.insertAdjacentElement('beforeend', title);
         survey.insertAdjacentElement('beforeend', comments);
         surveysWrapper.insertAdjacentElement('beforeend', survey);
     });
+}
+
+async function askForRemove(id, target) {
+    let removeAsk = prompt(`To remove ${id} one please type remove`, "");
+    if (removeAsk === "remove") {
+        const req = await postData(`/survey/remove/${id}`, {
+            "token": localStorage.getItem('token')
+        });
+        const res = await req.json();
+
+        if (res.error) {
+            alert(res.error);
+            return;
+        }
+        if (res.modifiedCount) {
+            target.remove();
+            alert('done!')
+        }
+    }
 }
 
 export {
